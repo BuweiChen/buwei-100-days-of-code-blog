72bf3189735ade72c1457d7f457c2007fc27630d
Buwei Chen <59496117+BuweiChen@users.noreply.github.com>
2024-05-22 16:41:33 -0500

    incorporate gray matter for parsing metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,35 @@
 import fs from "fs";
 import Link from "next/link";
+import matter from "gray-matter";
+import { PostMetadata } from "@/components/PostMetadata";
 
-const getPostMetadata = () => {
+const getPostMetadata = (): PostMetadata[] => {
   const folder = "posts/";
   const files = fs.readdirSync(folder);
   const markdownPosts = files.filter((file) => file.endsWith(".md"));
-  const slugs = markdownPosts.map((file) => file.replace(".md", ""));
-  return slugs;
+
+  const posts = markdownPosts.map((fileName) => {
+    const fileContent = fs.readFileSync(`posts/${fileName}`, "utf8");
+    const matterResult = matter(fileContent);
+    return {
+      title: matterResult.data.title,
+      date: matterResult.data.date,
+      subtitle: matterResult.data.subtitle,
+      slug: fileName.replace(".md", ""),
+    };
+  });
+  return posts;
 };
 
 const HomePage = () => {
   const postMetadata = getPostMetadata();
-  const postPreviews = postMetadata.map((slug) => (
+  const postPreviews = postMetadata.map((post) => (
     <div>
-      <Link href={`/posts/${slug}`}>
-        <h2>{slug}</h2>
+      <Link href={`/posts/${post.slug}`}>
+        <h2>{post.title}</h2>
       </Link>
+      <p>{post.subtitle}</p>
+      <p>{post.date}</p>
     </div>
   ));
   return <div>{postPreviews}</div>;
--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -1,11 +1,13 @@
 import fs from "fs";
 import Markdown from "markdown-to-jsx";
+import matter from "gray-matter";
 
 const getPostContent = (slug: string) => {
   const folder = "posts/";
   const file = `${folder}${slug}.md`;
   const content = fs.readFileSync(file, "utf8");
-  return content;
+  const matterResult = matter(content);
+  return matterResult.content;
 };
 
 const PostPage = (props: any) => {
