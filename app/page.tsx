dbbbe84047c6bc4e322c35d150364535a2fc77b2
Buwei Chen <59496117+BuweiChen@users.noreply.github.com>
2024-05-22 15:35:56 -0500

    implement getpostmetadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,15 @@
-const BlogPage = () => {
+import fs from "fs";
+
+const getPostMetadata = () => {
+  const folder = "posts/";
+  const files = fs.readdirSync(folder);
+  const markdownPosts = files.filter((file) => file.endsWith(".md"));
+  const slugs = markdownPosts.map((file) => file.replace(".md", ""));
+  return slugs;
+};
+
+const HomePage = () => {
+  const postMetadata = getPostMetadata();
   return <h1>Hello Blog!</h1>;
 };
-export default BlogPage;
+export default HomePage;
