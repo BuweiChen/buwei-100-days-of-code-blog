43788dab3b0a09c6bb80e7d777adc993fcd4ff76
Buwei Chen <59496117+BuweiChen@users.noreply.github.com>
2024-05-23 18:13:14 -0500

    add generate static param to create static path for each page

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,5 @@
-import fs from "fs";
 import Link from "next/link";
-import matter from "gray-matter";
-import { PostMetadata } from "@/components/PostMetadata";
-
-const getPostMetadata = (): PostMetadata[] => {
-  const folder = "posts/";
-  const files = fs.readdirSync(folder);
-  const markdownPosts = files.filter((file) => file.endsWith(".md"));
-
-  const posts = markdownPosts.map((fileName) => {
-    const fileContent = fs.readFileSync(`posts/${fileName}`, "utf8");
-    const matterResult = matter(fileContent);
-    return {
-      title: matterResult.data.title,
-      date: matterResult.data.date,
-      subtitle: matterResult.data.subtitle,
-      slug: fileName.replace(".md", ""),
-    };
-  });
-  return posts;
-};
+import getPostMetadata from "@/components/getPostMetadata";
 
 const HomePage = () => {
   const postMetadata = getPostMetadata();
--- posts/gpt-3-generated-poetry.md
+++ posts/gpt-3-generated-poetry.md
@@ -1,6 +1,6 @@
 ---
-title: "Using AI to generate poetry"
-subtitle: "I used GPT-3 to generate poetry and other creative content."
+title: "Example Title 2"
+subtitle: "Example subtitle 2."
 date: "2021-08-27"
 ---
 
